c44a2767ebfa4ab1d2ebc6ee43d71918d78f968e
Felipe-Leite-de-Souza <49239812+Felipe-Leite-de-Souza@users.noreply.github.com>
2022-01-30 16:42:19 -0300

    Folder creation and fundamentals exercises

---
+++ fundamentos/primeiro/go.mod
@@ -0,0 +1,3 @@
+module primeiro
+
+go 1.17
---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,16 @@
+// Programa executáveis iniciam pelo apcote main
+package main
+
+/*
+Os códigos em go são organizados em pacotes e para usá-los 
+é necessário declarar um ou mais variáveis
+*/
+
+import "fmt"
+
+
+// A porta de entrada de um programa GO é a função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
